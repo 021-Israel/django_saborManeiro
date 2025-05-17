c7542fd45a8cfffc7414b45d68254d75a7552092
Cauã Israel <caua.israel02@gmail.com>
2025-05-16 23:57:21 -0300

    refatoração página reservar

--- reservar/admin.py
+++ reservar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pessoa
 
-# Register your models here.
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email', 'data', 'convidados')
+    list_filter = ('data',)
+    search_fields = ('nome', 'sobrenome', 'email')
